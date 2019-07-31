08c1282dd490d7375bd113f8c42eeda4873e5703
mailtokartik1 <mailtokartik1@gmail.com>
2019-07-31 17:21:21 +0900

    add PDF viewer test

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('/Users/kartik_singh/Desktop/sampleElectron/pdfjs/web/viewer.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
